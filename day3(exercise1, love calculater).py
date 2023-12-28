e820e3ec0bc26f6a568c1451b900d927e57d2b9c
ashkandma <ashkanhasani31@gmail.com>
2023-12-28 04:59:34 +0330

    fix math error

--- day3(exercise1, love calculater).py
+++ day3(exercise1, love calculater).py
@@ -17,7 +17,7 @@ second_score_digit = l + o + v + e
 love_score = first_score_digit * 10 + second_score_digit
 if love_score <= 10 or love_score >= 90:
     print(f"Your score is {love_score} , you go together like coke and mentos.")
-elif love_score >= 40 or love_score <= 50:
+elif (love_score >= 40) and (love_score <= 50):
     print(f"Your score is {love_score} , you are all right together")
 else:
     print(f"Your score is {love_score}")
